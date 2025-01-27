e88e2f5c19f8d2d8547b0d73b2db8ce0a5b7f36c
Noah Bruns <nbruns@efs.at>
2025-01-27 14:27:07 +0100

    added rust exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::ffi::CStr;
-use std::os::raw::{c_void, c_char, c_uint};
 use std::mem::MaybeUninit;
+use std::os::raw::{c_char, c_uint, c_void};
+
+use rustsat::types::{Lit, TernaryVal};
+
+pub type PLA_F = Vec<(Vec<TernaryVal>, Vec<TernaryVal>)>;
 
 extern "C" {
     fn run_espresso_from_data(data: *const c_char, l: c_uint, out: *mut *mut c_char);
@@ -8,24 +12,117 @@ extern "C" {
     fn free(p: *mut c_void);
 }
 
-pub fn espresso(lines: &str) -> String {
+fn espresso(lines: &str) -> String {
     let bytes = lines.as_bytes();
     let mut buf = MaybeUninit::<*mut c_char>::uninit();
-    unsafe { run_espresso_from_data(bytes.as_ptr() as *const c_char, bytes.len() as u32, buf.as_mut_ptr()); }
+    unsafe {
+        run_espresso_from_data(
+            bytes.as_ptr() as *const c_char,
+            bytes.len() as u32,
+            buf.as_mut_ptr(),
+        );
+    }
     let c_str = Box::new(unsafe { buf.assume_init() });
-    let result = unsafe {CStr::from_ptr(*c_str).to_str().unwrap().to_owned() };
+    let result = unsafe { CStr::from_ptr(*c_str).to_str().unwrap().to_owned() };
     unsafe { free(*c_str as *mut c_void) };
     result
 }
 
-pub fn d1merge(lines: &str) -> String {
+fn d1merge(lines: &str) -> String {
     let bytes = lines.as_bytes();
     let mut buf = MaybeUninit::<*mut c_char>::uninit();
-    unsafe { run_d1merge_from_data(bytes.as_ptr() as *const c_char, bytes.len() as u32, buf.as_mut_ptr()); }
+    unsafe {
+        run_d1merge_from_data(
+            bytes.as_ptr() as *const c_char,
+            bytes.len() as u32,
+            buf.as_mut_ptr(),
+        );
+    }
     let c_str = Box::new(unsafe { buf.assume_init() });
-    let result = unsafe {CStr::from_ptr(*c_str).to_str().unwrap().to_owned() };
+    let result = unsafe { CStr::from_ptr(*c_str).to_str().unwrap().to_owned() };
     unsafe { free(*c_str as *mut c_void) };
     result
+}
+
+fn create_pla(F: PLA_F) -> string {
+    //implement function to convert a tuple of two vectors into anappropriate PLA string representation
+    let mut result = String::new();
+    result.push_str(".i ");
+    result.push_str(&(F[0].0.len().to_string()));
+    result.push_str("\n.o ");
+    result.push_str(&(F[0].1.len().to_string()));
+    result.push_str("\n.type f\n");
+    for f in F {
+        for i in 0..f.0.len() {
+            match f.0[i] {
+                TernaryVal::True => result.push('1'),
+                TernaryVal::False => result.push('0'),
+                TernaryVal::DontCare => result.push('-'),
+            }
+        }
+        result.push(' ');
+        for i in 0..f.1.len() {
+            match f.1[i] {
+                TernaryVal::True => result.push('1'),
+                TernaryVal::False => result.push('0'),
+                TernaryVal::DontCare => result.push('-'),
+            }
+        }
+        result.push('\n');
+    }
+    result.push_str(".e");
+    result
+}
+
+fn interpret_pla(pla: &str) -> PLA_F {
+    let mut result: PLA_F = Vec::new();
+    let lines: Vec<&str> = pla.lines().collect();
+    let mut inputs = 0;
+    let mut outputs = 0;
+    let mut type_line_index = 0;
+    for (i, l) in lines.iter().enumerate() {
+        if l.starts_with(".i") {
+            inputs = l[3..].parse().unwrap();
+        } else if l.starts_with(".o") {
+            outputs = l[3..].parse().unwrap();
+        } else if l.starts_with(".type") {
+            type_line_index = i;
+        }
+    }
+    for l in lines.iter().skip(type_line_index + 1) {
+        if l.starts_with(".e") {
+            break;
+        }
+        let mut parts: Vec<&str> = l.split(' ').collect();
+        let inputs_str = parts[0];
+        let outputs_str = parts[1];
+        let mut inputs_vec: Vec<TernaryVal> = Vec::new();
+        let mut outputs_vec: Vec<TernaryVal> = Vec::new();
+        for c in inputs_str.chars() {
+            match c {
+                '1' => inputs_vec.push(TernaryVal::True),
+                '0' => inputs_vec.push(TernaryVal::False),
+                '-' => inputs_vec.push(TernaryVal::DontCare),
+                _ => panic!("Invalid character in PLA file"),
+            }
+        }
+        for c in outputs_str.chars() {
+            match c {
+                '1' => outputs_vec.push(TernaryVal::True),
+                '0' => outputs_vec.push(TernaryVal::False),
+                '-' => outputs_vec.push(TernaryVal::DontCare),
+                _ => panic!("Invalid character in PLA file"),
+            }
+        }
+        result.push((inputs_vec, outputs_vec));
+    }
+    result
+}
+
+pub fn espresso(F: PLA_F) -> PLA_F {
+    let pla_string = create_pla(F);
+    let pla = espresso(&pla_string);
+    interpret_pla(&pla)
 }
 
 #[cfg(test)]
@@ -36,7 +133,7 @@ mod tests {
     #[test]
     #[serial]
     fn test_espresso_nothing_do_be_done() {
-    let input = r#".i 128
+        let input = r#".i 128
 .o 1
 .type f
 ----0-1-0----------------------------------------------------------------------------------------------------------------------- 1
@@ -53,8 +150,6 @@ mod tests {
         let result = espresso(input);
         assert_eq!(result, expected);
     }
-
-
 
     #[test]
     #[serial]
